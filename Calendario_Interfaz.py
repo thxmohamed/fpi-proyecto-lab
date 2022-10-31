465afaa0dc0b6782616e5a089b9049753657d4b9
VictorDuarteArce <115363165+VictorDuarteArce@users.noreply.github.com>
2022-10-31 16:14:33 -0300

    Add files via upload

---
+++ Calendario_Interfaz.py
@@ -0,0 +1,14 @@
+from tkinter import *
+root = Tk()
+#Lunes
+lunes = Frame(root)
+lunes.config(bg = "red", width = 100, height = 600)
+lunes.grid(row = 0, column = 0)
+#Martes
+martes = Frame(root)
+martes.config(bg = "lightblue", width = 100, height = 600)
+martes.grid(row = 0, column = 1)
+#Miércoles
+miercoles = Frame(root)
+miercoles.config(bg = "orange", width = 100, height = 600)
+miercoles.grid(row = 0, column = 2)
