55547a49ca96ca8020cba80d8057af3e5e35ba11
MOHAMED AL-MARZUK <115362866+thxmohamed@users.noreply.github.com>
2022-11-19 23:54:08 -0300

    Update test mio.py

--- test mio.py
+++ test mio.py
@@ -5,6 +5,105 @@ from tkinter.messagebox import *
 
 def cambia_ventana():
     root.withdraw()
+    horario1 =Toplevel()
+    horario1.title("Horario")
+    horario1.geometry("400x663")
+    
+    bloque1 = Frame(horario1)
+    textoB1 = Label(horario1, text = "Bloque 1", bg = "orange", font = ("Arial",14)).grid(row = 1, column = 0)
+    bloque1.config(bg = "orange", width = 100, height =  100)
+    bloque1.grid(row = 1, column = 0)
+
+    bloque2 = Frame(horario1)
+    textoB2 = Label(horario1, text = "Bloque 2", bg = "darkorange", font = ("Arial",14)).grid(row = 2, column = 0)
+    bloque2.config(bg = "darkorange", width = 100, height = 100)
+    bloque2.grid(row = 2, column =0)
+
+    bloque3 = Frame(horario1)
+    textoB3 = Label(horario1, text = "Bloque 3", bg = "orange", font = ("Arial",14)).grid(row = 3, column = 0)
+    bloque3.config(bg = "orange", width = 100, height = 100)
+    bloque3.grid(row = 3, column =0)
+
+    bloque4 = Frame(horario1)
+    textoB4 = Label(horario1, text = "Bloque 4", bg = "darkorange", font = ("Arial",14)).grid(row = 4, column = 0)
+    bloque4.config(bg = "darkorange", width = 100, height = 100)
+    bloque4.grid(row = 4, column =0)
+
+    bloque5 = Frame(horario1)
+    textoB5 = Label(horario1, text = "Bloque 5", bg = "orange", font = ("Arial",14)).grid(row = 5, column = 0)
+    bloque5.config(bg = "orange", width = 100, height = 100)
+    bloque5.grid(row = 5, column =0)
+
+    bloque6 = Frame(horario1)
+    textoB6 = Label(horario1, text = "Bloque 6", bg = "darkorange", font = ("Arial",14)).grid(row = 6, column = 0)
+    bloque6.config(bg = "darkorange", width = 100, height = 100)
+    bloque6.grid(row = 6, column =0)
+
+    # Aquí abajo se mostrará cada bloque de cada día correspondiente, generando como un sistema
+    # de coordenadas.
+
+    #Lunes
+
+    lunes = Frame(horario1)
+    textoL = Label(horario1, text = "Lunes", bg = "orange", font = ("Arial",14)).grid(row = 0, column = 1)
+    lunes.config(bg = "orange", width = 150, height =  30)
+    lunes.grid(row = 0, column = 1)
+    
+    lunes1 = Frame(horario1)
+    lunes1.config(bg = "lightgreen", width = 150, height = 100)
+    lunes1.grid(row = 1, column = 1)
+
+    lunes2 = Frame(horario1)
+    lunes2.config(bg = "lightblue", width = 150, height = 100)
+    lunes2.grid(row = 2, column = 1)
+
+    lunes3 = Frame(horario1)
+    lunes3.config(bg = "lightgreen", width = 150, height = 100)
+    lunes3.grid(row = 3, column = 1)
+
+    lunes4 = Frame(horario1)
+    lunes4.config(bg = "lightblue", width = 150, height = 100)
+    lunes4.grid(row = 4, column = 1)
+
+    lunes5 = Frame(horario1)
+    lunes5.config(bg = "lightgreen", width = 150, height = 100)
+    lunes5.grid(row = 5, column = 1)
+
+    lunes6 = Frame(horario1)
+    lunes6.config(bg = "lightblue", width = 150, height = 100)
+    lunes6.grid(row = 6, column = 1)
+
+
+    #Martes
+    martes1 = Frame(horario1)
+    martes1.config(bg = "lightblue", width = 150, height = 100)
+    martes1.grid(row = 1, column = 2)
+
+    martes2 = Frame(horario1)
+    martes2.config(bg = "lightgreen", width = 150, height = 100)
+    martes2.grid(row = 2, column = 2)
+
+    martes3 = Frame(horario1)
+    martes3.config(bg = "lightblue", width = 150, height = 100)
+    martes3.grid(row = 3, column = 2)
+
+    martes4 = Frame(horario1)
+    martes4.config(bg = "lightgreen", width = 150, height = 100)
+    martes4.grid(row = 4, column = 2)
+
+    martes5 = Frame(horario1)
+    martes5.config(bg = "lightblue", width = 150, height = 100)
+    martes5.grid(row = 5, column = 2)
+
+    martes6 = Frame(horario1)
+    martes6.config(bg = "lightgreen", width = 150, height = 100)
+    martes6.grid(row = 6, column = 2)
+
+    martes = Frame(horario1)
+    textoM = Label(horario1, text = "Martes", bg = "darkorange", font = ("Arial",14)).grid(row = 0, column = 2)
+    martes.config(bg = "darkorange", width = 150, height = 30)
+    martes.grid(row = 0, column =2)
+
     
     def siguiente():
         
@@ -103,11 +202,113 @@ def cambia_ventana():
         jueves6.config(bg = "lightgreen", width = 150, height = 100)
         jueves6.grid(row = 6, column = 2)
 
+        def siguiente2():
+                
+            horario2.withdraw()
+            horario3=Toplevel()
+            horario3.title("Horario")
+            horario3.geometry("400x663")
+
+            bloque1 = Frame(horario3)
+            textoB1 = Label(horario3, text = "Bloque 1", bg = "orange", font = ("Arial",14)).grid(row = 1, column = 0)
+            bloque1.config(bg = "orange", width = 100, height =  100)
+            bloque1.grid(row = 1, column = 0)
+
+            bloque2 = Frame(horario3)
+            textoB2 = Label(horario3, text = "Bloque 2", bg = "darkorange", font = ("Arial",14)).grid(row = 2, column = 0)
+            bloque2.config(bg = "darkorange", width = 100, height = 100)
+            bloque2.grid(row = 2, column =0)
+
+            bloque3 = Frame(horario3)
+            textoB3 = Label(horario3, text = "Bloque 3", bg = "orange", font = ("Arial",14)).grid(row = 3, column = 0)
+            bloque3.config(bg = "orange", width = 100, height = 100)
+            bloque3.grid(row = 3, column =0)
+
+            bloque4 = Frame(horario3)
+            textoB4 = Label(horario3, text = "Bloque 4", bg = "darkorange", font = ("Arial",14)).grid(row = 4, column = 0)
+            bloque4.config(bg = "darkorange", width = 100, height = 100)
+            bloque4.grid(row = 4, column =0)
+
+            bloque5 = Frame(horario3)
+            textoB5 = Label(horario3, text = "Bloque 5", bg = "orange", font = ("Arial",14)).grid(row = 5, column = 0)
+            bloque5.config(bg = "orange", width = 100, height = 100)
+            bloque5.grid(row = 5, column =0)
+
+            bloque6 = Frame(horario3)
+            textoB6 = Label(horario3, text = "Bloque 6", bg = "darkorange", font = ("Arial",14)).grid(row = 6, column = 0)
+            bloque6.config(bg = "darkorange", width = 100, height = 100)
+            bloque6.grid(row = 6, column =0)
+
+            #Viernes
+
+            viernes = Frame(horario3)
+            textoV = Label(horario3, text = "Viernes", bg = "orange", font = ("Arial",14)).grid(row = 0, column = 1)
+            viernes.config(bg = "orange", width = 150, height = 30)
+            viernes.grid(row = 0, column = 1)
+
+            viernes1 = Frame(horario3)
+            viernes1.config(bg = "lightgreen", width = 150, height = 100)
+            viernes1.grid(row = 1, column = 1)
+
+            viernes2 = Frame(horario3)
+            viernes2.config(bg = "lightblue", width = 150, height = 100)
+            viernes2.grid(row = 2, column = 1)
+
+            viernes3 = Frame(horario3)
+            viernes3.config(bg = "lightgreen", width = 150, height = 100)
+            viernes3.grid(row = 3, column = 1)
+
+            viernes4 = Frame(horario3)
+            viernes4.config(bg = "lightblue", width = 150, height = 100)
+            viernes4.grid(row = 4, column = 1)
+
+            viernes5 = Frame(horario3)
+            viernes5.config(bg = "lightgreen", width = 150, height = 100)
+            viernes5.grid(row = 5, column = 1)
+
+            viernes6 = Frame(horario3)
+            viernes6.config(bg = "lightblue", width = 150, height = 100)
+            viernes6.grid(row = 6, column = 1)
+
+            #Sábado
+            sabado1 = Frame(horario3)
+            sabado1.config(bg = "lightblue", width = 150, height = 100)
+            sabado1.grid(row = 1, column = 2)
+
+            sabado = Frame(horario3)
+            textoS = Label(horario3, text = "Sábado", bg = "darkorange", font = ("Arial",14)).grid(row = 0, column = 2)
+            sabado.config(bg = "darkorange", width = 150, height = 30)
+            sabado.grid(row = 0, column =2)
+
+            sabado2 = Frame(horario3)
+            sabado2.config(bg = "lightgreen", width = 150, height = 100)
+            sabado2.grid(row = 2, column = 2)
+
+            sabado3 = Frame(horario3)
+            sabado3.config(bg = "lightblue", width = 150, height = 100)
+            sabado3.grid(row = 3, column = 2)
+
+            sabado4 = Frame(horario3)
+            sabado4.config(bg = "lightgreen", width = 150, height = 100)
+            sabado4.grid(row = 4, column = 2)
+
+            sabado5 = Frame(horario3)
+            sabado5.config(bg = "lightblue", width = 150, height = 100)
+            sabado5.grid(row = 5, column = 2)
+
+            sabado6 = Frame(horario3)
+            sabado6.config(bg = "lightgreen", width = 150, height = 100)
+            sabado6.grid(row = 6, column = 2)
+            
+            boton = Button(horario3, text = "Agregar", bg= "cyan", font = ("Arial",11), padx=17.5, pady= 2.1, command = ventana_entra_act).grid(row=0, column = 0)
+            boton_cerrar = Button(horario3, text = "Finalizar Programa", command = root.destroy, bg = "red", fg = "white", font = ("Arial", 12)).grid(row = 7, column = 1)
+            boton_atras = Button(horario3, text = "Atrás", bg= "cyan", font = ("Arial",11), padx= 27.4, pady= 2.1, command = siguiente).grid(row=7, column = 0)
+
         boton = Button(horario2, text = "Agregar", bg= "cyan", font = ("Arial",11), padx=17.5, pady= 2.1, command = ventana_entra_act).grid(row=0, column = 0)
         boton_cerrar = Button(horario2, text = "Finalizar Programa", command = root.destroy, bg = "red", fg = "white", font = ("Arial", 12)).grid(row = 7, column = 1)
-        boton_siguiente = Button(horario2, text = "Siguiente", bg= "cyan", font = ("Arial",11), padx= 39.4, pady= 2.1, command = siguiente).grid(row=7, column = 2)
+        boton_siguiente = Button(horario2, text = "Siguiente", bg= "cyan", font = ("Arial",11), padx= 39.4, pady= 2.1, command = siguiente2).grid(row=7, column = 2)
         boton_atras = Button(horario2, text = "Atrás", bg= "cyan", font = ("Arial",11), padx= 27.4, pady= 2.1, command = cambia_ventana).grid(row=7, column = 0)
-        
+            
 #Esta segunda función creará la ventana dedicada a ingresar las actividades.
     def ventana_entra_act():
         ventana_entrada=Toplevel()
@@ -181,183 +382,7 @@ def cambia_ventana():
 
     # Asignacion de bloques, se visualizarán en la columna de la izquierda e indicarán el bloque
     # horario correspondiente.
-    horario1 =Toplevel()
-    horario1.title("Horario")
-    horario1.geometry("400x663")
-    
-    bloque1 = Frame(horario1)
-    textoB1 = Label(horario1, text = "Bloque 1", bg = "orange", font = ("Arial",14)).grid(row = 1, column = 0)
-    bloque1.config(bg = "orange", width = 100, height =  100)
-    bloque1.grid(row = 1, column = 0)
-
-    bloque2 = Frame(horario1)
-    textoB2 = Label(horario1, text = "Bloque 2", bg = "darkorange", font = ("Arial",14)).grid(row = 2, column = 0)
-    bloque2.config(bg = "darkorange", width = 100, height = 100)
-    bloque2.grid(row = 2, column =0)
-
-    bloque3 = Frame(horario1)
-    textoB3 = Label(horario1, text = "Bloque 3", bg = "orange", font = ("Arial",14)).grid(row = 3, column = 0)
-    bloque3.config(bg = "orange", width = 100, height = 100)
-    bloque3.grid(row = 3, column =0)
-
-    bloque4 = Frame(horario1)
-    textoB4 = Label(horario1, text = "Bloque 4", bg = "darkorange", font = ("Arial",14)).grid(row = 4, column = 0)
-    bloque4.config(bg = "darkorange", width = 100, height = 100)
-    bloque4.grid(row = 4, column =0)
-
-    bloque5 = Frame(horario1)
-    textoB5 = Label(horario1, text = "Bloque 5", bg = "orange", font = ("Arial",14)).grid(row = 5, column = 0)
-    bloque5.config(bg = "orange", width = 100, height = 100)
-    bloque5.grid(row = 5, column =0)
-
-    bloque6 = Frame(horario1)
-    textoB6 = Label(horario1, text = "Bloque 6", bg = "darkorange", font = ("Arial",14)).grid(row = 6, column = 0)
-    bloque6.config(bg = "darkorange", width = 100, height = 100)
-    bloque6.grid(row = 6, column =0)
-
-    # Aquí abajo se mostrará cada bloque de cada día correspondiente, generando como un sistema
-    # de coordenadas.
-
-    #Lunes
-    
-    lunes1 = Frame(horario1)
-    lunes1.config(bg = "lightgreen", width = 150, height = 100)
-    lunes1.grid(row = 1, column = 1)
-
-    lunes2 = Frame(horario1)
-    lunes2.config(bg = "lightblue", width = 150, height = 100)
-    lunes2.grid(row = 2, column = 1)
-
-    lunes3 = Frame(horario1)
-    lunes3.config(bg = "lightgreen", width = 150, height = 100)
-    lunes3.grid(row = 3, column = 1)
-
-    lunes4 = Frame(horario1)
-    lunes4.config(bg = "lightblue", width = 150, height = 100)
-    lunes4.grid(row = 4, column = 1)
-
-    lunes5 = Frame(horario1)
-    lunes5.config(bg = "lightgreen", width = 150, height = 100)
-    lunes5.grid(row = 5, column = 1)
-
-    lunes6 = Frame(horario1)
-    lunes6.config(bg = "lightblue", width = 150, height = 100)
-    lunes6.grid(row = 6, column = 1)
-
-
-    #Martes
-    martes1 = Frame(horario1)
-    martes1.config(bg = "lightblue", width = 150, height = 100)
-    martes1.grid(row = 1, column = 2)
-
-    martes2 = Frame(horario1)
-    martes2.config(bg = "lightgreen", width = 150, height = 100)
-    martes2.grid(row = 2, column = 2)
-
-    martes3 = Frame(horario1)
-    martes3.config(bg = "lightblue", width = 150, height = 100)
-    martes3.grid(row = 3, column = 2)
-
-    martes4 = Frame(horario1)
-    martes4.config(bg = "lightgreen", width = 150, height = 100)
-    martes4.grid(row = 4, column = 2)
-
-    martes5 = Frame(horario1)
-    martes5.config(bg = "lightblue", width = 150, height = 100)
-    martes5.grid(row = 5, column = 2)
-
-    martes6 = Frame(horario1)
-    martes6.config(bg = "lightgreen", width = 150, height = 100)
-    martes6.grid(row = 6, column = 2)
-
-    '''
-
-    #Viernes
-    viernes1 = Frame(root2)
-    viernes1.config(bg = "lightgreen", width = 100, height = 100)
-    viernes1.grid(row = 1, column = 5)
-
-    viernes2 = Frame(root2)
-    viernes2.config(bg = "lightblue", width = 100, height = 100)
-    viernes2.grid(row = 2, column = 5)
-
-    viernes3 = Frame(root2)
-    viernes3.config(bg = "lightgreen", width = 100, height = 100)
-    viernes3.grid(row = 3, column = 5)
-
-    viernes4 = Frame(root2)
-    viernes4.config(bg = "lightblue", width = 100, height = 100)
-    viernes4.grid(row = 4, column = 5)
-
-    viernes5 = Frame(root2)
-    viernes5.config(bg = "lightgreen", width = 100, height = 100)
-    viernes5.grid(row = 5, column = 5)
-
-    viernes6 = Frame(root2)
-    viernes6.config(bg = "lightblue", width = 100, height = 100)
-    viernes6.grid(row = 6, column = 5)
-
-
-
-    #Sábado
-    sabado1 = Frame(root2)
-    sabado1.config(bg = "lightblue", width = 100, height = 100)
-    sabado1.grid(row = 1, column = 6)
-
-    sabado2 = Frame(root2)
-    sabado2.config(bg = "lightgreen", width = 100, height = 100)
-    sabado2.grid(row = 2, column = 6)
-
-    sabado3 = Frame(root2)
-    sabado3.config(bg = "lightblue", width = 100, height = 100)
-    sabado3.grid(row = 3, column = 6)
-
-    sabado4 = Frame(root2)
-    sabado4.config(bg = "lightgreen", width = 100, height = 100)
-    sabado4.grid(row = 4, column = 6)
-
-    sabado5 = Frame(root2)
-    sabado5.config(bg = "lightblue", width = 100, height = 100)
-    sabado5.grid(row = 5, column = 6)
-
-    sabado6 = Frame(root2)
-    sabado6.config(bg = "lightgreen", width = 100, height = 100)
-    sabado6.grid(row = 6, column = 6)
-    '''
     #Asignación de días, se mostrará el nombre del día a lo largo de la primera fila
-
-
-    lunes = Frame(horario1)
-    textoL = Label(horario1, text = "Lunes", bg = "orange", font = ("Arial",14)).grid(row = 0, column = 1)
-    lunes.config(bg = "orange", width = 150, height =  30)
-    lunes.grid(row = 0, column = 1)
-
-    martes = Frame(horario1)
-    textoM = Label(horario1, text = "Martes", bg = "darkorange", font = ("Arial",14)).grid(row = 0, column = 2)
-    martes.config(bg = "darkorange", width = 150, height = 30)
-    martes.grid(row = 0, column =2)
-
-    '''
-    miercoles = Frame(root2)
-    textoW = Label(root2, text = "Miércoles", bg = "orange", font = ("Arial",14)).grid(row = 0, column = 3)
-    miercoles.config(bg = "orange", width = 100, height = 30)
-    miercoles.grid(row = 0, column =3)
-
-    jueves = Frame(root2)
-    textoJ = Label(root2, text = "Jueves", bg = "darkorange", font = ("Arial",14)).grid(row = 0, column = 4)
-    jueves.config(bg = "darkorange", width = 100, height = 30)
-    jueves.grid(row = 0, column =4)
-
-    viernes = Frame(root2)
-    textoV = Label(root2, text = "Viernes", bg = "orange", font = ("Arial",14)).grid(row = 0, column = 5)
-    viernes.config(bg = "orange", width = 100, height = 30)
-    viernes.grid(row = 0, column =5)
-
-    sabado = Frame(root2)
-    textoV = Label(root2, text = "Sábado", bg = "darkorange", font = ("Arial",14)).grid(row = 0, column = 6)
-    sabado.config(bg = "darkorange", width = 100, height = 30)
-    sabado.grid(row = 0, column =6)
-    '''
     # Definición del botón, al hacer click, este llevará a la ventana para ingresar actividad
 
 
